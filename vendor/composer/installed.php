1ba3fe08ab486e0e5b3e531d85e7ca95522bd925
icekunx <icekunxp@gmail.com>
2021-02-13 22:57:49 +0700

    heroku-line

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9d9a0d889c4a83b4521327882bf60be38b476e3',
+    ),
+  ),
+);
